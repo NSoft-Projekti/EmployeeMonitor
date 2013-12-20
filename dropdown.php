97bb4cee2a4248e98eb3c908d73f15b908dfa195
Martina <Home@Home-PC>
2013-12-20 10:46:03 +0100

    Drop down list za gradove

---
+++ dropdown.php
@@ -0,0 +1,15 @@
+ <select name="select" class="textfields" id="ddlCity">
+
+<option id="0">--Select City--</option>
+
+<?php 
+include 'indeks.php';
+header('Content-type: text/plain; charset=utf-8');
+
+	$getAllCities = mysql_query("SELECT * FROM gradovi;");
+	while($viewAllCities=mysql_fetch_array($getAllCities)){
+?>
+<option id="<php echo $viewAllCities['gradID'];?>"><?php echo $viewAllCities['naziv'] ?></option>
+<?php } ?>
+
+</select>

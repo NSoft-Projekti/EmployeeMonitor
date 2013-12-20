b5e0a1f001a7cc85f457ea158eb0cce2d9b9a885
Marina <Marina@Marina-PC>
2013-12-20 09:53:57 +0100

    prva stranica

---
+++ prva_stranica.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Insert title here</title>
+<link type="text/css" href="prva_stranica.css" rel="stylesheet"/>
+</head>
+<body>
+<div class="container">
+	<div class="form">
+	<form method="POST" action="login.php">
+		<div class="headline">
+			<h1>Log in</h1>
+		</div>	
+		<div class="name">
+					<input type="text" name="username" placeholder="Username"/>
+				</div>
+		<div id="password">
+					<input type="password" name="password" placeholder="Password"/>
+		</div>
+		<div class="login">
+			<a href="navigacija.html"><input type="submit" name="submit" value="Log in"></a>
+		</div>
+	</form>
+	</div>
+</div>
+</body>
+</html>

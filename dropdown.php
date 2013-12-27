f76200313a703b2314a5badc45fd501e3845ea51
Martina <Home@Home-PC>
2013-12-27 10:25:40 +0100

    Drop down lista

--- dropdown.php
+++ dropdown.php
@@ -1,10 +1,12 @@
-<select name="select" class="textfields" id="ddlCity">
+
+ <select name="select" class="textfields" id="ddlCity">
+ 
 
 <option id="0">--Select City--</option>
 
 <?php 
-include 'indeks.php';
 header('Content-type: text/plain; charset=utf-8');
+include 'indeks.php';
 
 	$getAllCities = mysql_query("SELECT * FROM gradovi;");
 	while($viewAllCities=mysql_fetch_array($getAllCities)){

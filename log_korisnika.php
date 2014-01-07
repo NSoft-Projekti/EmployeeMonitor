f8813c1763e7e530f126946b411b73bd6cbd59e8
ninafsr <nina.djinkic@gmail.com>
2014-01-07 14:02:39 +0100

    kada se postojeći korisnik logira

--- log_korisnika.php
+++ log_korisnika.php
@@ -7,46 +7,46 @@
 <style>
 ul
 {
-	list-style-type:none;
-	margin:0;
-	padding:0;
-	overflow:hidden;
+list-style-type:none;
+margin:0;
+padding:0;
+overflow:hidden;
 }
 li
 {
-	float:left;
+float:left;
 }
 a:link,a:visited
 {
-	display:block;
-	width:400px;
-	font-weight:bold;
-	color:black;
-	background-color:#D8D8D8;
-	text-align:center;
-	padding:4px;
-	text-decoration:none;
-	text-transform:uppercase;
+display:block;
+width:400px;
+font-weight:bold;
+color:black;
+background-color:#D8D8D8;
+text-align:center;
+padding:4px;
+text-decoration:none;
+text-transform:uppercase;
 }
 a:hover,a:active
 {
-	background-color:#C0C0C0;
+background-color:#C0C0C0;
 }
 
 </style>
 </head>
 <body>
 <div class="container">
-<div class="header">
-
+	<div class="header">
+		
 <ul>
 <li><a href="#osobni_podaci">Osobni podaci</a></li>
 <li><a href="#pregled_vremena">Pregled radnog vremena</a></li>
 </ul>
-
-</div>
-<div class="registracija">
-</div>
+		
+	</div>
+	<div class="registracija">
+	</div>
 </div>
 </body>
 </html>

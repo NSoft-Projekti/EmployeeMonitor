f539326b79f642f26a7214f10baed09460e76a53
Mirna <Win7@MirnaPC>
2014-01-07 13:06:24 +0100

    Za Harisa

--- Trazi.php
+++ Trazi.php
@@ -1,36 +1,93 @@
+<style>
+.Table
+{
+/*border: 1px solid #BBBBBB;*/
+border:none;
+/*border-collapse: collapse;*/
+padding: 5px;
+}
+
+.Table th
+{
+	
+background-color:#D4ECF7;
+padding: 8px 12px;
+font-family:Arial;
+font-size:11px;
+}
+
+.Table td
+{
+
+padding: 0 5px;
+/*border-right: 1px solid #BBBBBB;*/ 
+height: 30px;
+
+}
+.Table .input {
+/*border: 1px solid #BBBBBB;*/ 
+background:#FFFFFF;
+width: 215px;
+height: 20px;
+}
+</style>
+
+
+
 <?php
 include ('indeks.php');
 
-
-$query = $_GET['query'];
+$query = $_POST['query'];
 $min_length = 3;
 
  if(strlen($query) >= $min_length){ 
 	 
 	$query = htmlspecialchars($query);
 	$query = mysql_real_escape_string($query);
-	$query ="hari";
 
 	
-	$raw_results = mysql_query("SELECT * FROM zaposlenici WHERE (`korisnicko_ime` LIKE '%".$query."%')  OR (`ime` = '%".$query."%')  OR (`prezime` LIKE '%".$query."%')")or die(mysql_error());
+	$raw_results = mysql_query("SELECT * FROM zaposlenici
+            WHERE (`korisnicko_ime` LIKE '%".$query."%')  OR (`ime` = '%".$query."%')  OR (`prezime` LIKE '%".$query."%')") or die(mysql_error());
 	 
-	if(mysql_num_rows($raw_results) > 0){ 
+	if(mysql_num_rows($raw_results) > 0)
+	{ 
 		 $output="";
-		while($results = mysql_fetch_array($raw_results)){
-			echo "<p><h3>".$results['korisnicko_ime']."</h3>".$results['ime']."</p>";
-			}
-				
-			}
+		 echo "<table class='Table' border='1px'>";
+		 
+		 while($results = mysql_fetch_array($raw_results))
+		 {
+		 	{
+		 
+		 
+		 		echo "<tr>";
+		 		echo "<td>" . "Korisnicko ime" . "</td>";
+		 		echo "<td>" . "Ime" . "</td>";
+		 		echo "<td>" . "Prezime" . "</td>";
+		 		echo "<td>" . "Adresa" . "</td>";
+		 		echo "<td>" . "Spol" . "</td>";
+		 		echo "<td>" . "E-mail" . "</td>";
+		 		echo "<td>" . "Datum registracije" . "</td>";
+		 		echo "</tr>";
+		 
+		 		echo "<tr>";
+		 		echo "<td>" .htmlspecialchars($results['korisnicko_ime'] ). "</td>";
+		 		echo "<td>" .htmlspecialchars($results['ime']) . "</td>";
+		 		echo "<td>" .htmlspecialchars($results['prezime'] ). "</td>";
+		 		echo "<td>" .htmlspecialchars($results['adresa']) . "</td>";
+		 		echo "<td>" .htmlspecialchars($results['spol'] ). "</td>";
+		 		echo "<td>" .htmlspecialchars($results['email']) . "</td>";
+		 		echo "<td>" .htmlspecialchars($results['datum_registracije']) . "</td>";
+		 		echo "</tr>";
+		 
+		 	}
+		 	echo "</table> ";
+		 }
+	}
 			else{ 
 				echo "No results";
 			}
 			
-			}
-			else{ // if query length is less than minimum
-				echo "Minimum length is ".$min_length;
-			}
-			
-?>
-			
-
 		
+ }
+			?>
+			

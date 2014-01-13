b96bb975b7ddf3ab6cbfcd42957430f2d5104a56
Marina <Marina@Marina-PC>
2014-01-13 12:09:10 +0100

    navigacija

--- navigacija.php
+++ navigacija.php
@@ -4,22 +4,23 @@
 <meta charset="utf-8">
 <title>Insert title here</title>
 <link type="text/css" href="navigacija.css" rel="stylesheet"/>
+<link type="text/css" href="registracija1.css" rel="stylesheet"/>
 </head>
 <body>
 <div class="container">
 	<div class="header">
-		
-		<a href="registracija.html">Registracija</a>
-		<a href="lista_korisnika.html">Lista korisnika</a>
-		
+	<ul>
+		<li><a href="registracija.php">Registracija</a></li>
+		<li><a href="search.php">Lista korisnika</a></li>
+        <li><a href="logout.php">Log out</a></li>
+        </ul>
 	</div>
 	<div class="registracija">
 	</div>
 	
 	
 	
-<form action="logout.php" method="post">
-	<input type="submit" value="Log out" /></form>
+
 	
 </div>
 </body>

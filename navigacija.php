e0776479cb6a9f9ebdf71c83f48c4b953462112c
ninafsr <nina.djinkic@gmail.com>
2014-01-16 10:54:44 +0100

    navigacija

--- navigacija.php
+++ navigacija.php
@@ -1,27 +1,40 @@
 <!DOCTYPE html>
 <html>
 <head>
-<meta charset="utf-8">
-<title>Insert title here</title>
-<link type="text/css" href="navigacija.css" rel="stylesheet"/>
-<link type="text/css" href="registracija1.css" rel="stylesheet"/>
+    <meta charset="utf-8">
+    <title>Insert title here</title>
+    <link type="text/css" href="navigacija.css" rel="stylesheet"/>
+
 </head>
 <body>
 <div class="container">
-	<div class="header">
-	<ul>
-		<li><a href="registracija.php">Registracija</a></li>
-		<li><a href="search.php">Lista korisnika</a></li>
-        <li><a href="logout.php">Log out</a></li>
+    <div class="header">
+        <ul>
+            <li><a href="#">Korisnici</a>
+                <ul>
+                    <li><a href="#">Registriraj korisnika</a></li>
+                </ul>
+                <ul>
+                    <li><a href="#">Pregled svih korisnika</a></li>
+                </ul>
+            </li>
+            <li><a href="#">Prostorije</a>
+                <ul>
+                    <li><a href="#">Dodaj novu prostoriju</a></li>
+                </ul>
+                <ul>
+                    <li><a href="#">Pregled svih prostorija</a></li>
+                </ul>
+            </li>
         </ul>
-	</div>
-	<div class="registracija">
-	</div>
-	
-	
-	
+    </div>
+    <div class="registracija">
+    </div>
 
-	
+
+
+
+
 </div>
 </body>
 </html>

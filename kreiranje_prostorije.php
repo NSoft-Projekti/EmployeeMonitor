7fe94ea022b870c8f8b097a5ac155c2cd7e59f58
ninafsr <nina.djinkic@gmail.com>
2014-01-16 14:10:30 +0100

    kreiranje_prostorije

--- kreiranje_prostorije.php
+++ kreiranje_prostorije.php
@@ -13,36 +13,36 @@
 
     <div class="header">
         <ul>
-            <li><a href="#">Korisnici</a>
+            <li><a href="#">Users</a>
                 <ul>
-                    <li><a href="registracija.php">Registriraj korisnika</a></li>
+                    <li><a href="registracija.php">Register</a></li>
                 </ul>
                 <ul>
-                    <li><a href="#">Pregled svih korisnika</a></li>
+                    <li><a href="#">User list</a></li>
                 </ul>
             </li>
-            <li><a href="#">Prostorije</a>
+            <li><a href="#">Rooms</a>
                 <ul>
-                    <li><a href="kreiranje_prostorije.php">Dodaj novu prostoriju</a></li>
+                    <li><a href="kreiranje_prostorije.php">Create new room</a></li>
                 </ul>
                 <ul>
-                    <li><a href="lista_prostorija.php">Pregled svih prostorija</a></li>
+                    <li><a href="lista_prostorija.php">Room list</a></li>
                 </ul>
             </li>
         </ul>
     </div>
     <div class="naslov">
-        <h1>Kreiranje prostorije</h1>
+        <h1>Create new room</h1>
     </div>
     <div class="naziv">
-        <input type="text" id="name" name="name" size="30" placeholder="Naziv prostorije"/>
+        <input type="text" id="name" name="name" placeholder="Name"/>
     </div>
     <div class="limit">
         Limit:
     </div>
     <div class="vrijeme">
         <select name="sat" id="sat" style="width:70px;">
-            <option value="sat">Vrijeme:</option>
+            <option value="sat">Time:</option>
             <?php
             for ($i=15; $i<=20; $i++) {
                 ?>
@@ -54,11 +54,11 @@
             ?>
         </select>
     </div>
-    <div class="opis">
-        <textarea name="comment" form="comment" >Opis:</textarea>
+    <div class="opis">Description:
+       <textarea name="Komentar" rows=4 cols=25 wrap=physical></textarea>
     </div>
     <div class="registrirajse">
-        <form action="#"> <input type="submit" name="button2" value="Dodaj"></form>
+        <form action="#"> <input type="submit" id="button2" name="button2" value="Add"></form>
     </div>
 
 </div>

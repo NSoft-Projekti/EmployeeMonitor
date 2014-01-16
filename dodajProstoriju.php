27189402ccb17f502f56ed0fc2b5577fe10948d1
hp probook <hp probook@hpprobook-PC>
2014-01-16 12:01:26 +0100

    dodavanje prostorije

---
+++ dodajProstoriju.php
@@ -0,0 +1,42 @@
+<?php
+include_once "indeks.php";
+
+if(!empty($_POST['title']) and !empty($_POST['state']) and !empty($_POST['limitation']) and !empty($_POST['description'])){
+
+	$name = $_POST['title'];
+	//$state= $_POST['state'];
+	$sat = $_POST['limitation'];
+	$comment = $_POST['description'];
+
+	$checkroom = mysql_query("SELECT * FROM room WHERE naziv = '".$room."'");
+    
+    if(mysql_num_rows($checkroom) == 1){
+    	echo "<h1>Greska</h1>";
+    	echo "<p>Ta prostorija je vec u bazi.</p>";
+    }
+    else {
+    	$dodajprostorijurquery = mysql_query("INSERT INTO rooms (title,state,limitation,description) 
+    			VALUES('$name', '$_POST('1'),  '$sat', '$comment')");
+            
+        
+        if($dodajprostorijurquery)
+        {
+            echo "<h1>Uspjeh</h1>";
+            echo "<p>Uspjesno ste se dodali prostoriju u bazu.</p>";
+        }
+        else
+        {
+            echo "<h1>Greska</h1>";
+            echo "<p>Prostoriju nije moguce dodati u bazu.</p>";   
+        }  
+    }
+}
+else {
+	if($_SERVER['REQUEST_METHOD'] == 'POST'){
+		echo '<script>alert("Greska polja su prazna");</script>';
+		//header ('Location dodajProstoriju.php');
+		echo '<META HTTP-EQUIV="Refresh" Content="0; URL=dodajProstoriju.php">';  // Header nije htjelo radi pa radi redirect na ovaj nacin.
+	}
+ }
+ 
+?>

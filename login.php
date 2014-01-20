37b25eb612c384f4daea67c1b7cef8f020601de6
ninafsr <nina.djinkic@gmail.com>
2014-01-20 13:48:22 +0100

    login

--- login.php
+++ login.php
@@ -6,31 +6,30 @@ include_once 'indeks.php';
 
 if(!empty($_SESSION['LoggedIn']) and !empty($_SESSION['Username']))
 {
- 
-echo '<h1>Samo za elanove</h1>
+
+    echo '<h1>Samo za elanove</h1>
      <p>Hvala za logiranje! Vi ste <b></b> a Vaša email adresa je: <b></b>.</p>';
- 
+
 }
 elseif(!empty($_POST['username']) and !empty($_POST['password']))
 {
     $username = mysql_real_escape_string($_POST['username']);
     $password = mysql_real_escape_string($_POST['password']);
- 
+
     $checklogin = mysql_query("SELECT * FROM zaposlenici WHERE korisnicko_ime = '$username' AND lozinka = '$password'");
-    
-    
+
+
     if(mysql_num_rows($checklogin) == 1)
     {
         $row = mysql_fetch_array($checklogin);
         $email = $row['EmailAddress'];
         $admin = $row['administrator'];
- 		@session_start();
- 		
+        @session_start();
         $_SESSION['Username'] = $username;
         $_SESSION['Emailaddress'] = $email;
         $_SESSION['LoggedIn'] = 1;
         $_SESSION['Admin'] = 1;
- 
+
         if($admin == 1){
             // Redirect Admin
             //header( 'Location: navigacija.php' );
@@ -46,9 +45,10 @@ elseif(!empty($_POST['username']) and !empty($_POST['password']))
     }
     else
     {
-        echo "<h1>Greška</h1>";
-        echo "<p>Klik na  <a href=\"login.php\">i probajte ponovno!</a></p>";
+        echo "<script type='text/javascript'>alert('Invalid ID or password.Please try again');</script>";
+
+        echo "<script type='text/javascript'>window.location.href='prva_stranica.php'</script>";
     }
 }
-    
+
 ?>

a6eb07b01cbce2cb7af2e4883819227000568b8c
Mirna <Win7@MirnaPC>
2014-01-20 10:26:05 +0100

    Delete room

---
+++ DeleteRoom.php
@@ -0,0 +1,22 @@
+<?php
+include 'indeks.php';
+
+$room_id=$_GET['r_id'];
+
+$sql="DELETE FROM rooms WHERE roomID='$room_id'";
+$result=mysql_query($sql);
+
+if ($result)
+{
+	header("Location: Search_room.php");
+	//echo "Deleted Successfully";
+	//echo "<br>";
+	//echo "<a href='Search.php'> Back to search page</a>";
+}
+else
+{
+	echo "ERROR!";
+	// close connection
+	mysql_close();
+}
+?>

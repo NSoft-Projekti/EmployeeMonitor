459a5f75856ee6639019fac0b02a142b797a4696
Marina <Marina@Marina-PC>
2014-01-20 10:34:20 +0100

    navigation

---
+++ navigation.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Insert title here</title>
+<link type="text/css" href="navigation.css" rel="stylesheet"/>
+
+</head>
+<body>
+<div class="container">
+	<div class="header">
+		<ul>
+			<li><a href="#">Users</a>
+				<ul>
+					<li><a href="#">Register user</a></li>
+				</ul>
+				<ul>
+					<li><a href="#">Users rewiev</a></li>
+				</ul>
+			</li>
+			<li><a href="#">Rooms</a>
+				<ul>
+					<li><a href="#">Add new room</a></li>
+				</ul>
+				<ul>
+					<li><a href="#">Rooms rewiev</a></li>
+				</ul>
+			</li>
+		</ul>
+	</div>
+	<div class="registration">
+	</div>
+	
+	
+	
+
+	
+</div>
+</body>
+</html>

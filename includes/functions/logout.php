c21c0a1197f632cd44e63c5c8ae7deac8a7ee5aa
Martina <Home@Home-PC>
2014-01-24 15:03:03 +0100

    Update logout

--- includes/functions/logout.php
+++ includes/functions/logout.php
@@ -1,21 +1,11 @@
 <?php
-
 include '../indeks.php';
 
-
-session_start();
-
-if(!isset($_REQUEST['logmeout'])){
-	echo "<center>Are you sure you want to logout?</center><br />";
-	echo "<center><a href=logout.php?logmeout>Yes</a> | <a href=javascript:history.back()>No</a>";
-} else {
-	session_destroy();
-	if(!session_is_registered('first_name')){
-		echo "<center><font color=red><strong>You are now logged out!</strong></font></center><br />";
-		
-		include '../../Home.php';
-	}
-}
+session_start ();
+session_destroy ();
+header ("location: /EmployeeMonitor/Home.php");
+	
+	
 ?>
 
 

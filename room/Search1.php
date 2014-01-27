baa12077f4e56227866256e9547d7a5aba51664f
Martina <Home@Home-PC>
2014-01-27 11:23:45 +0100

    Promjene u Search1

--- room/Search1.php
+++ room/Search1.php
@@ -1,12 +1,10 @@
-
 <?php include_once '../includes/indeks.php'; ?>
-
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html>
 <head>
-    <link type="text/css" href="../assets/css/registration.css" rel="stylesheet"/>
-    <style>
-        .Table
+ <link type="text/css" href="../assets/css/registration.css" rel="stylesheet"/>
+ <style>
+.Table
         {
             border:none;
             padding: 5px;
@@ -37,9 +35,9 @@
             width: 215px;
             height: 20px;
         }
-    </style>
+</style>
 </head>
-<body>
+<body> 
 
 <div class="container" margin-top="30px;">
     <div class="header">
@@ -66,91 +64,94 @@
                 </ul>
     </div>
 
-    <form action="Search.php" method="post">
-        <div class="regrm">
-            <select name="room" class="textfields" id="room">
 
-                <option id="0">Select room</option>
+<form action="" method="post">
+<div class="regrm">
+Select room:  <select name="room" class="textfields" id="room">
 
-                <?php
-                $getAllRooms = mysql_query("SELECT * FROM rooms;");
-                while($viewAllRooms=mysql_fetch_array($getAllRooms)){
-                    echo "<option value=".$viewAllRooms['roomID'].">".$viewAllRooms['title']."</option>";
-                }
-                ?>
+<option id="0">--Select--</option>
+ 
+<?php 
+        $getAllRooms = mysql_query("SELECT * FROM rooms;");
+        while($viewAllRooms=mysql_fetch_array($getAllRooms)){
+        echo "<option value=".$viewAllRooms['RoomID'].">".$viewAllRooms['Name']."</option>";
+   }
+?>
 
-            </select>
-            <input type="submit" name="Search_rm" value="Send"/>
-        </div>
-    </form>
-
-
-
-    <?php
-    include ('../includes/indeks.php');
-
-    $query_room = $_POST['room'];
+</select>
+<input type="submit" name="Search_rm" />
+</div>
+</form> 
 
 
-    $query_room = htmlspecialchars($query_room);
-    $query_room = mysql_real_escape_string($query_room);
+ 
+ <?php
+include_once ('../includes/indeks.php');
 
+$query_room = $_POST['room'];
 
-
-
+ 
+        $query_room = htmlspecialchars($query_room);
+        $query_room = mysql_real_escape_string($query_room);
+        
+        
+                
+    
     if(empty($query_room)==false)
     {
-        $query=("SELECT * FROM rooms WHERE roomID = '$query_room' ");
+            $query=("SELECT * FROM rooms WHERE RoomID = '$query_room' ");
     }
-    else
+     else
     {
-        $query="SELECT * FROM rooms";
+            $query="SELECT * FROM rooms";
     }
-
+    
     $raw_results = mysql_query($query);
-
-    if(mysql_num_rows($raw_results) > 0)
-    {
-        $output="";
-        echo "<table class='Table' border='1px'>";
-
-        echo "<tr bgcolor='#CAEDF7'>";
-        echo "<td>" . "Title" . "</td>";
-        echo "<td>" . "State" . "</td>";
-        echo "<td>" . "Limitation" . "</td>";
-        echo "<td>" . "Description" . "</td>";
-        echo "</tr>";
-
-        while($results = mysql_fetch_array($raw_results))
-
-        {
-
-            {
-                echo "<form action='DeleteRoom.php' method ='GET'>";
-
-                echo "<tr>";
-                echo "<td>" .htmlspecialchars($results['title'] ). "</td>";
-                echo "<td>" .htmlspecialchars($results['state']) . "</td>";
-                echo "<td>" .htmlspecialchars($results['limitation'] ). "</td>";
-                echo "<td>" .htmlspecialchars($results['description'] ). "</td>";
-
-
-                echo "<input type='hidden' name='r_id' value=".$results['roomID'].">";
-                echo "<td> <input type='submit' value='Delete'/></td>";
-                echo "</form>";
-
+    
+        if(mysql_num_rows($raw_results) > 0)
+        { 
+                $output="";
+                echo "<table class='Table' border='1px'>";
+                
+                echo "<tr bgcolor='#CAEDF7'>";
+                echo "<td>" . "Name" . "</td>";
+                echo "<td>" . "Limit" . "</td>";
+                echo "<td>" . "Description" . "</td>";
                 echo "</tr>";
-
-            }
-
+                
+                while($results = mysql_fetch_array($raw_results))
+                        
+                {
+                
+                        {
+                                                  echo "<form action='functions/DeleteRoom.php' method ='GET'>";
+                                                  
+                                 echo "<tr>";
+                                 echo "<td>" .htmlspecialchars($results['Name'] ). "</td>";
+                                 echo "<td>" .htmlspecialchars($results['Limit'] ). "</td>";
+                                 echo "<td>" .htmlspecialchars($results['Description'] ). "</td>";
+                                 
+                                 
+                echo "<input type='hidden' name='r_id' value=".$results['RoomID'].">";
+                echo "<td> <input type='submit' value='Delete'/></td>"; 
+                 echo "</form>";
+                 
+                 echo "<form action='Update.php' method='GET'>";
+                 echo "<input type='hidden' name='r_id' value=".$results['RoomID'].">";
+                 echo "<td> <input type='submit' value='Update'/></td>";
+                 echo "</form>";
+                                 
+                                 echo "</tr>";
+                 
+                         }
+                 
+                 }
+                 echo "</table> ";
         }
-        echo "</table> ";
-    }
 
-
-
-    ?>
-
-</div>
+                
+ 
+                        ?>
+                        
 </body>
 </html>

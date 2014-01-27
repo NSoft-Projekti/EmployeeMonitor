c301800c1d3bb6781fca08f8d7ae31d40ac792b2
ninafsr <nina.djinkic@gmail.com>
2014-01-27 10:12:07 +0100

    Search.php

--- employee/Search.php
+++ employee/Search.php
@@ -1,3 +1,45 @@
+
+<style>
+
+    .Table
+    {
+        /*border: 1px solid #BBBBBB;*/
+        border:none;
+        /*border-collapse: collapse;*/
+        padding: 5px;
+
+    }
+
+    .Table th
+    {
+
+        background-color:red;
+        padding: 8px 12px;
+        font-family:Arial;
+        font-size:11px;
+        margin-top:20px;
+    }
+
+    .Table td
+    {
+
+        padding: 0 5px;
+        /*border-right: 1px solid #BBBBBB;*/
+        height: 30px;
+
+    }
+    .Table .input {
+        /*border: 1px solid #BBBBBB;*/
+        background:blue;
+        width: 215px;
+        height: 20px;
+    }
+
+
+
+
+</style>
+
 <?php require_once '../includes/indeks.php'; ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -6,176 +48,128 @@
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <link type="text/css" href="../assets/css/administration.css" rel="stylesheet"/>
 
-<style>
-
-.Table
-{
-/*border: 1px solid #BBBBBB;*/
-border:none;
-/*border-collapse: collapse;*/
-padding: 5px;
-
-}
-
-.Table th
-{
-	
-background-color:red;
-padding: 8px 12px;
-font-family:Arial;
-font-size:11px;
-margin-top:20px;
-}
-
-.Table td
-{
-
-padding: 0 5px;
-/*border-right: 1px solid #BBBBBB;*/ 
-height: 30px;
-
-}
-.Table .input {
-/*border: 1px solid #BBBBBB;*/ 
-background:blue;
-width: 215px;
-height: 20px;
-}
-body{
-font-family:calibri;
-background:url(../assets/img/pozadina.jpg);
-width:1024px;
-height:600px;
-margin:auto;
-
-}
-
-
-
-</style>
 </head>
 <body>
-<!-- <div class="container">-->
+<div class="container">
 
-<div class="header">
-	<ul>
-		<li><a href="#">Employees</a>
-			<ul>
-				<li><a href="Registration.php">Register employee</a></li>
-			</ul>
-			<ul>
-				<li><a href="Search.php">Search employees</a></li>
-			</ul>
-		</li>
-		<li><a href="#">Rooms</a>
-			<ul>
-				<li><a href="../room/Add.php">Add new room</a></li>
-			</ul>
-			<ul>
-				<li><a href="../room/Search.php">Room list</a></li>
-			</ul>
-		</li>
-        <li><a href="../includes/functions/logout.php">Log out</a></li>
-	</ul>
-</div>
+    <div class="header">
 
-<form action="Search.php" method="post">
+        <ul>
+            <li><a href="#">Employees</a>
+                <ul>
+                    <li><a href="Registration.php">Register employee</a></li>
+                </ul>
+                <ul>
+                    <li><a href="Search.php">Search employees</a></li>
+                </ul>
+            </li>
+            <li><a href="#">Rooms</a>
+                <ul>
+                    <li><a href="../room/Add.php">Add new room</a></li>
+                </ul>
+                <ul>
+                    <li><a href="../room/Search.php">Room list</a></li>
+                </ul>
+            </li>
+            <li><a href="../includes/functions/logout.php">Log out</a></li>
+        </ul>
 
-<div class="regrm">
-Radno mjesto:  <select name="position" class="textfields" id="position">
+    </div>
+    <div class="img">
+        <form name="name" action='Search.php' method="POST">
+            <div class="submit">
+                <input type="text" name="query" placeholder="Username" id="user" />
+            </div>
+            <div class="regrm">
+                <select name="radno_mjesto" class="textfields" id="radno_mjesto">
 
-<option id="0" value=0>--Select--</option>
-<?php 
-	$getAllRadnaMjesta = mysql_query("SELECT * FROM positions;");
-	while($viewAllRadnaMjesta=mysql_fetch_array($getAllRadnaMjesta)){
-	echo "<option value=".$viewAllRadnaMjesta['PositionID'].">".$viewAllRadnaMjesta['Name']."</option>";
-   }
-?>
+                    <option id="0">Radno mjesto</option>
+                    <?php
+                    $getAllRadnaMjesta = mysql_query("SELECT * FROM radna_mjesta;");
+                    while($viewAllRadnaMjesta=mysql_fetch_array($getAllRadnaMjesta)){
+                        ?>
+                        <option value="<?php echo $viewAllRadnaMjesta['radno_mjestoID']?>"><?php echo $viewAllRadnaMjesta['naziv'] ?></option>
+                    <?php } ?>
 
-</select>
-</div>
-<input type="text" name="name" />
-<input type="submit" name="Search" />
-</form> 
+                </select>
+            </div>
+            <div class="send">
+                <input type="submit" name="submit" value="Send"/>
+            </div>
+        </form>
+    </div>
 
- 
-
-<?php
-
-$txtName = $_POST['name'];
-$pos = $_POST['position'];
-
-//$min_length = 3;
-$query="";
-
-if(empty($txtName)==false and empty($pos)==false)
-{
-	$query="SELECT * FROM employees WHERE (PositionID = '$pos' AND FirstName = '$txtName') ";
-}
-else if(empty($txtName)==true and empty($pos)==false)
-{
-	$query="SELECT * FROM employees WHERE PositionID = '$pos'";
-}
-else if(empty($txtName)==false and empty($pos)==true)
-{
-	$query="SELECT * FROM employees WHERE FirstName = '$txtName'";
-}
-else{
-	$query="SELECT * FROM employees";
-}
-$raw_results = mysql_query($query);
-
- //if(strlen($query) >= $min_length){ 
-	 
-	$txtIme = htmlspecialchars($txtIme);
-	$txtIme = mysql_real_escape_string($txtIme);
-	
-	if(mysql_num_rows($raw_results) > 0)
-	{ 
-		$output="";
-		echo "<table class='Table' border='1px'>";
-		
-		echo "<tr bgcolor='6699CC'>";
-		echo "<td>" . "Username" . "</td>";
-		echo "<td>" . "First name" . "</td>";
-		echo "<td>" . "Last name" . "</td>";
-		echo "<td>" . "Address" . "</td>";
-		echo "<td>" . "Gender" . "</td>";
-		echo "<td>" . "E-mail" . "</td>";
-		echo "<td>" . "Registration date" . "</td>";
-		echo "</tr>";
-		
-		while($results = mysql_fetch_array($raw_results))
-			
-		{
-		
-			{
-				  		echo "<form action='functions/deleteZaposlenikById.php' method ='GET'>";
-				  		
-		 		echo "<tr>";
-		 		echo "<td>" .htmlspecialchars($results['Username'] ). "</td>";
-		 		echo "<td>" .htmlspecialchars($results['FirstName']) . "</td>";
-		 		echo "<td>" .htmlspecialchars($results['LastName'] ). "</td>";
-		 		echo "<td>" .htmlspecialchars($results['Address']) . "</td>";
-		 		echo "<td>" .htmlspecialchars($results['Gender'] ). "</td>";
-		 		echo "<td>" .htmlspecialchars($results['Email']) . "</td>";
-		 		echo "<td>" .htmlspecialchars($results['RegistrationDate']) . "</td>";
-		 		
-		echo "<input type='hidden' name='z_id' value=".$results['EmployeeID'].">";
-		 		echo "<td><input type='submit' name='submit' value='Delete'></td>"; 
-		 		echo "</form>";
-		 		
-		 		echo "</tr>";
-		 
-		 	}
-		 
-		 }
-		 echo "</table> ";
-	
-		
- }
-			?>
-		
 
 </body>
-</html>	
+</html>
+<?php
+include ('indeks.php');
+
+$query = $_POST['query'];
+$query1 = $_POST['naziv'];
+$min_length = 3;
+
+
+
+if(strlen($query) >= $min_length){
+
+    $query = htmlspecialchars($query);
+    $query = mysql_real_escape_string($query);
+
+    $query1 = htmlspecialchars($query1);
+    $query1 = mysql_real_escape_string($query1);
+
+
+    $raw_results = mysql_query("SELECT * FROM zaposlenici
+			WHERE (`korisnicko_ime` LIKE '%".$query."%')  OR (`ime` = '%".$query."%')  OR (`prezime` LIKE '%".$query."%')") or die(mysql_error());
+
+    if(mysql_num_rows($raw_results) > 0)
+    {
+        $output="";
+        echo "<table class='Table' border='1px'>";
+
+        echo "<tr bgcolor='6699CC'>";
+        echo "<td>" . "Korisnicko ime" . "</td>";
+        echo "<td>" . "Ime" . "</td>";
+        echo "<td>" . "Prezime" . "</td>";
+        echo "<td>" . "Adresa" . "</td>";
+        echo "<td>" . "Spol" . "</td>";
+        echo "<td>" . "E-mail" . "</td>";
+        echo "<td>" . "Datum registracije" . "</td>";
+        echo "</tr>";
+
+        while($results = mysql_fetch_array($raw_results))
+
+        {
+
+            {
+                echo "<form action='deleteZaposlenikById.php' method ='GET'>";
+
+                echo "<tr>";
+                echo "<td>" .htmlspecialchars($results['korisnicko_ime'] ). "</td>";
+                echo "<td>" .htmlspecialchars($results['ime']) . "</td>";
+                echo "<td>" .htmlspecialchars($results['prezime'] ). "</td>";
+                echo "<td>" .htmlspecialchars($results['adresa']) . "</td>";
+                echo "<td>" .htmlspecialchars($results['spol'] ). "</td>";
+                echo "<td>" .htmlspecialchars($results['email']) . "</td>";
+                echo "<td>" .htmlspecialchars($results['datum_registracije']) . "</td>";
+
+                echo "<input type='hidden' name='z_id' value=".$results['zaposlenikID'].">";
+                echo "<td><input type='submit' name='submit' value='Delete'></td>";
+                echo "</form>";
+
+                echo "</tr>";
+
+            }
+
+        }
+        echo "</table> ";
+    }
+    else{
+        echo "No results";
+    }
+
+}
+?>
+
+

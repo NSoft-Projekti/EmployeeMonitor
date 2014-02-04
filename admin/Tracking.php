9469aa5894a5fd1844ab4c7c04dc477d073f6a41
Hari <Hari@Hari-PC>
2014-02-04 23:03:56 +0100

    Trenutno logirani

---
+++ admin/Tracking.php
@@ -0,0 +1,58 @@
+<?php include '../includes/indeks.php';
+session_start();
+
+$query = "SELECT * FROM employeemonitor.rooms";
+$result = mysql_query($query) or die (mysql_error());
+
+while ($row=mysql_fetch_array($result))
+{
+	//echo $row['RoomID'];
+	echo "<table class='Table' border='1px'>";
+	
+	echo "<tr bgcolor='#CAEDF7'>";
+	echo "<td>" .htmlspecialchars($row['Name'] )."</td>";
+	
+	
+	$r_query="SELECT * FROM (select * from employeemonitor.employeerooms where RoomID=".$row['RoomID']." and LoggedIn='1' order by LogTime desc ) as emprooms_tmp	group by EmployeeID order by LogTime desc";
+	$raw_results = mysql_query($r_query);
+	while($res = mysql_fetch_array($raw_results))
+	{
+		//if($row['RoomID']==$res['RoomID'])
+		//{
+		$empName = mysql_query("SELECT Username FROM employees WHERE EmployeeID = ".$res['EmployeeID']."");
+		$resEmpName = mysql_fetch_array($empName);
+			echo "<tr>";
+			echo "<td>" .htmlspecialchars($resEmpName['Username']). "</td>";
+			echo "<td>" .htmlspecialchars($res['LogTime'] ). "</td>";
+			//echo "<td>" .htmlspecialchars($res['LoggedIn'] ). "</td>";
+			echo "</tr>";
+		//}
+	}
+	echo "</tr>";
+	
+	echo "</table> ";
+	
+$to = 'hary.bb@gmail.com';
+$subject = 'test email';
+$body = 'body email';
+
+$headers = 'From: nekog <someone@tim9.com>';
+
+mail('hary.bb@gmail.com', 'Mercury test mail', 'If you can read this, everything was fine!');
+/*
+if(mail('hary.bb@gmail.com', 'Mercury test mail', 'If you can read this, everything was fine!')mail($to,$subject,$body,$headers)){
+	echo 'poslan mail';
+}else{
+	echo 'greska :/';
+
+}*/
+}
+// $row = mysql_fetch_array ( $result );
+// $currEmpId=$row['EmployeeID'];
+// $query=("SELECT * FROM employeerooms WHERE EmployeeID = '$currEmpId'");
+
+
+
+
+
+?>
